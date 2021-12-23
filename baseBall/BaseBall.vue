8acf62de7957f62dfd67bccf509613ff75b70477
credevo <credevo@gmail.com>
2021-12-23 20:43:40 +0900

    vue init

---
+++ baseBall/BaseBall.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+       <h1> 야구 게임 </h1>
+       <form @submit="onSubmit">
+           <input :value="value" />
+       </form>
+       {{result}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                value : '',
+                result : '',
+            }
+        },
+        methods : {
+            onSubmit(e){
+                e.preventDefault();
+            }
+        }
+        
+    }
+</script>
--- baseBall/webpack.config.js
+++ baseBall/webpack.config.js
@@ -1,3 +1,8 @@
+
+
+const path  = require('path');
+
+
 // node의 모듈 만들기
 // webpacking~ start
 // script를 하나로 뭉쳐주는 역할 = webpack
@@ -11,10 +16,10 @@ module.exports = {
         // 합칠때 규칙을 적는다.
         rules : [{}],
     },
-    // 부가적인 기능
+    // 부가적인 기능 
     plugin :[],
     output : {
         filename : '[name].js', //  = 'app.js',
-        path : './dist' // 폴더 경로 dist폴더안에 app.js이 최종 결과물로 나오게 된다.
+        path : 'dist' // 폴더 경로 dist폴더안에 app.js이 최종 결과물로 나오게 된다.
     },
 }
