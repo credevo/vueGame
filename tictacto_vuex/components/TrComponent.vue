38d8c25308189a7ec5da883e65edb4d0ab094d5a
credevo <credevo@gmail.com>
2022-01-08 22:42:30 +0900

    tictacto created by evenbus

---
+++ tictacto_vuex/components/TrComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <tr>
+        <td-component v-for="(cellData,index) in rowData" :key="index" 
+            :cell-data="cellData"  
+            :row-index="rowIndex" 
+            :cell-index="index"
+            ></td-component>
+    </tr>
+            <!-- @td-click="$emit('td-click',$event)"
+            @td-test="$emit('td-test',$event)" -->
+</template>
+
+<script>
+import TdComponent from './TdComponent.vue'
+export default {
+    name : 'tr-component',
+    props : {
+        rowData : Array,
+        rowIndex : Number,
+    },
+    components: { TdComponent },
+    
+}
+</script>
